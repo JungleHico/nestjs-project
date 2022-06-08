6ba24f67223460424e2c5dc25e08abb82bbd30ba
JungleHico <scnu_zhuxu@163.com>
2022-06-08 10:21:14 +0800

    bcrypt 加密

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { UserService } from 'src/modules/user/user.service';
 import { JwtService } from '@nestjs/jwt';
 import { AuthDto } from './dto/auth.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -12,7 +13,11 @@ export class AuthService {
 
   async login({ username, password }: AuthDto): Promise<any> {
     const user = await this.userService.findOneByUsername(username);
-    if (!user || user.password !== password) {
+    if (!user) {
+      throw new BadRequestException('帐号不存在或密码错误');
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
       throw new BadRequestException('帐号不存在或密码错误');
     }
 
@@ -24,9 +29,13 @@ export class AuthService {
   }
 
   async register({ username, password }: AuthDto) {
+    // 加密
+    const saltOrRounds = 10;
+    const hash = await bcrypt.hash(password, saltOrRounds);
+
     const createUser = {
       username,
-      password,
+      password: hash,
       nickname: '',
       avatar: '',
     };
